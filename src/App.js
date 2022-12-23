911531c292a96ddd9c3d5f8117c1a2a73bb23da8
aron-helu <aronmilahela@gmail.com>
2022-12-24 01:18:27 +0300

    Add route to the about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 import './App.css';
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
     </header>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   </div>
 );
